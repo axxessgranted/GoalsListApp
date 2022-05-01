65631b12417e5db7cac34f44d08db1cb1f4f95b9
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-05-01 20:57:50 +0200

    Refactor App.js

--- App.js
+++ App.js
@@ -1,23 +1,12 @@
 import { useState } from 'react';
-import {
-  Button,
-  FlatList,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
+import GoalInput from './components/GoalInput';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-  const [goalText, setGoalText] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
 
-  const goalInputHandler = (enteredText) => {
-    setGoalText(enteredText);
-  };
-
-  const addGoalHandler = () => {
+  const addGoalHandler = (goalText) => {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
       { text: goalText, key: `${goalText}-${Math.random().toString()}` },
@@ -26,24 +15,12 @@ export default function App() {
 
   return (
     <View style={styles.appContainer}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Your course goal!'
-          onChangeText={goalInputHandler}
-        />
-        <Button title='Add Goal' onPress={addGoalHandler} />
-      </View>
+      <GoalInput addGoalHandler={addGoalHandler} />
       <View style={styles.goalsContainer}>
         <FlatList
           data={courseGoals}
           renderItem={(goalData) => {
-            return (
-              /* Extra View to support rounded corners for iOS */
-              <View style={styles.goalItem}>
-                <Text style={styles.goalText}>{goalData.item.text}</Text>
-              </View>
-            );
+            return <GoalItem text={goalData.item.text} />;
           }}
         />
       </View>
@@ -57,32 +34,8 @@ const styles = StyleSheet.create({
     paddingTop: 48,
     paddingHorizontal: 16,
   },
-  inputContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc',
-  },
-  textInput: {
-    borderWidth: 1,
-    borderColor: '#cccccc',
-    width: '70%',
-    marginRight: 8,
-    padding: 8,
-  },
+
   goalsContainer: {
     flex: 5,
-  },
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc',
-  },
-  goalText: {
-    color: 'white',
   },
 });
