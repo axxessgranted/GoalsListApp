a9aeafe86862129ae06a7ca102bee230f2edd28a
Grant Kantor <58207637+axxessgranted@users.noreply.github.com>
2022-05-02 15:37:50 +0200

    Add android ripple effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,12 +2,15 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 const GoalItem = ({ id, text, deleteItemHandler }) => {
   return (
-    <Pressable onPress={deleteItemHandler.bind(this, id)}>
-      {/* Extra View to support rounded corners for iOS */}
-      <View style={styles.goalItem}>
+    /* Extra View to support rounded corners for iOS */
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        onPress={deleteItemHandler.bind(this, id)}
+      >
         <Text style={styles.goalText}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
@@ -16,11 +19,12 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
+
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
   goalText: {
     color: 'white',
+    padding: 8,
   },
 });
